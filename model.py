ec49ff575bdcf5dd352442b9c182dd8a022c752a
John Bang <johnybang@gmail.com>
2017-04-16 20:40:54 +0000

    trained LeNet5 on sample data with Early Stopping

--- model.py
+++ model.py
@@ -3,28 +3,38 @@ import numpy as np
 import os
 import cv2
 
-# Load driving data to dataframe
+# Load relevant driving data to dataframe
 df = pd.read_csv('data/driving_log.csv',
-                 names=['center_path', 'left_path', 'right_path', 'steering', 'throttle', 'brake', 'speed'])
-
-# Append corresponding image objects to dataframe
-for cam in ['center','left','right']:
-    df[cam + '_image'] = [cv2.imread(os.path.join('data', p.strip())) for p in df[cam + '_path']]
+                 names=['center_path', 'left_path', 'right_path', 'steering', 'throttle', 'brake', 'speed'],
+                 usecols=['center_path','steering'])
 
 # Create training data
-X_train = np.array(df.center_image.tolist())
-y_train = df.speed.values
+X_train = np.array([cv2.imread(os.path.join('data', p.strip())) for p in df.center_path])
+y_train = df.steering.values
 
 from keras.models import Sequential
-from keras.layers import Flatten, Dense
+from keras.layers import Flatten, Dense, Lambda
+from keras.callbacks import EarlyStopping, ModelCheckpoint
+from keras.layers.convolutional import Convolution2D
+from keras.layers.pooling import MaxPooling2D
+
 
 model = Sequential()
-model.add(Flatten(input_shape=(160, 320, 3)))
+model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160, 320, 3)))
+model.add(Convolution2D(6, 5, 5, activation='relu'))
+model.add(MaxPooling2D())
+model.add(Convolution2D(6, 5, 5, activation='relu'))
+model.add(MaxPooling2D())
+model.add(Flatten())
+model.add(Dense(120))
+model.add(Dense(84))
 model.add(Dense(1))
 
-model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=7)
 
-model.save('model.h5')
+model.compile(loss='mse', optimizer='adam')
+callbacks = [EarlyStopping(monitor='val_loss', patience=5),
+             ModelCheckpoint(filepath='model.h5', verbose=1, save_best_only=True)]
+hist = model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=100, callbacks=callbacks)
+print(hist.history)
 
 import gc; gc.collect()
