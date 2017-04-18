eedb46ef3693ac734fa1359267f2289ba622f796
John Bang <johnybang@gmail.com>
2017-04-18 07:45:22 +0000

    starting to add back data one set at a time

--- model.py
+++ model.py
@@ -5,8 +5,8 @@ import cv2
 
 fieldnames = ['center_path', 'left_path', 'right_path',
               'steering', 'throttle', 'brake', 'speed']
-datadirs = ['CenteredCounterClockwise2Laps', 'CenteredClockwise2Laps',
-            'CounterClockwiseRecovery1Lap', 'ClockwiseRecovery1Lap',
+datadirs = ['CenteredCounterClockwise2Laps',#, 'CenteredClockwise2Laps',
+            #'CounterClockwiseRecovery1Lap', 'ClockwiseRecovery1Lap',
             'FailureCorrections']
 
 # Load relevant driving data to dataframes, folder at a time
@@ -26,6 +26,17 @@ df = pd.concat(df_list, ignore_index=True)
 # Construct training arrays
 X_train = np.array([cv2.imread(p) for p in df.center_path])
 y_train = df.steering.values
+
+# Left and right camera data with correction factor
+lr_correction = 0.2
+mask = df.datadir.isin(['CenteredCounterClockwise2Laps'])#, 'CenteredClockwise2Laps'])
+X_train_left = np.array([cv2.imread(p) for p in df.loc[mask, 'left_path']])
+X_train_right = np.array([cv2.imread(p) for p in df.loc[mask, 'right_path']])
+y_train_left = df.loc[mask, 'steering'].values + lr_correction
+y_train_right = df.loc[mask, 'steering'].values - lr_correction
+# Append it
+X_train = np.concatenate((X_train, X_train_left, X_train_right))
+y_train = np.concatenate((y_train, y_train_left, y_train_right))
 
 # rec_df = pd.read_csv('RecoveryLaps/driving_log.csv', names=fieldnames, usecols=['center_path', 'steering'])
 
@@ -63,13 +74,17 @@ from keras import backend as K
 from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda, Cropping2D, Dropout
 from keras.callbacks import EarlyStopping, ModelCheckpoint
-from keras.layers.convolutional import Convolution2D
+from keras.layers.convolutional import Conv2D
 from keras.layers.pooling import MaxPooling2D
+from keras.regularizers import l2
 
 
 arch = 'nvidia'
 keep_prob_conv = 1.0
 keep_prob_fc = 0.5
+l2_conv = None #l2(0.0)
+l2_fc = None #l2(0.0)
+loss_func = 'mse'
 
 model = Sequential()
 model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160, 320, 3)))
@@ -78,37 +93,42 @@ model.add(Cropping2D(cropping=((70, 25), (0, 0)))) # crop out top 75 pixels and 
 if arch == 'lenet5':
 
     # LeNet5 Architecture
-    model.add(Convolution2D(6, 5, 5, activation='relu'))
+    model.add(Conv2D(6, 5, 5, activation='relu', W_regularizer=l2_conv))
     model.add(MaxPooling2D())
     model.add(Dropout(keep_prob_conv))
-    model.add(Convolution2D(6, 5, 5, activation='relu'))
+    model.add(Conv2D(6, 5, 5, activation='relu', W_regularizer=l2_conv))
     model.add(MaxPooling2D())
     model.add(Dropout(keep_prob_fc))
     model.add(Flatten())
-    model.add(Dense(120))
+    model.add(Dense(120, W_regularizer=l2_fc))
     model.add(Dropout(keep_prob_fc))
-    model.add(Dense(84))
+    model.add(Dense(84, W_regularizer=l2_fc))
     model.add(Dropout(keep_prob_fc))
 
 elif arch == 'nvidia':
 
     # Nvidia Architecture
-    model.add(Convolution2D(24, 5, 5, subsample=(2, 2), activation='relu'))
+    model.add(Conv2D(24, 5, 5, subsample=(2, 2), activation='relu',
+                     W_regularizer=l2_conv))
     model.add(Dropout(keep_prob_conv))
-    model.add(Convolution2D(36, 5, 5, subsample=(2, 2), activation='relu'))
+    model.add(Conv2D(36, 5, 5, subsample=(2, 2), activation='relu',
+                     W_regularizer=l2_conv))
     model.add(Dropout(keep_prob_conv))
-    model.add(Convolution2D(48, 5, 5, subsample=(2, 2), activation='relu'))
+    model.add(Conv2D(48, 5, 5, subsample=(2, 2), activation='relu',
+                     W_regularizer=l2_conv))
     model.add(Dropout(keep_prob_conv))
-    model.add(Convolution2D(64, 3, 3, activation='relu'))
+    model.add(Conv2D(64, 3, 3, activation='relu',
+                     W_regularizer=l2_conv))
     model.add(Dropout(keep_prob_conv))
-    model.add(Convolution2D(64, 3, 3, activation='relu'))
+    model.add(Conv2D(64, 3, 3, activation='relu',
+                     W_regularizer=l2_conv))
     model.add(Dropout(keep_prob_fc))
     model.add(Flatten())
-    model.add(Dense(100))
+    model.add(Dense(100, W_regularizer=l2_fc))
     model.add(Dropout(keep_prob_fc))
-    model.add(Dense(50))
+    model.add(Dense(50, W_regularizer=l2_fc))
     model.add(Dropout(keep_prob_fc))
-    model.add(Dense(10))
+    model.add(Dense(10, W_regularizer=l2_fc))
     model.add(Dropout(keep_prob_fc))
 
 else:
@@ -117,7 +137,7 @@ else:
 
 model.add(Dense(1))
 
-model.compile(loss='mse', optimizer='adam')
+model.compile(loss=loss_func, optimizer='adam')
 callbacks = [EarlyStopping(monitor='val_loss', patience=5),
              ModelCheckpoint(filepath='model.h5', verbose=1, save_best_only=True)]
 hist = model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=100, callbacks=callbacks)
